a9ef11c2a1125b92ec457e9e64ce478f5e7a351c
Sasha <sasha361322@gmail.com>
2017-05-23 23:58:03 +0300

    add web

---
+++ src/main/java/ru/shipilov/config/Conf.java
@@ -0,0 +1,31 @@
+package ru.shipilov.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@Configuration
+//@EnableAutoConfiguration
+//@ComponentScan("ru.cinimex.scheduler")
+@EnableJpaRepositories
+@EnableTransactionManagement
+@EnableWebMvc
+public class Conf extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+}
